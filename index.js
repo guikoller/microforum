e95ed9ad9d1823b1255295a85fa53d8f9a54763b
guikoller <g_koller@hotmail.com>
2022-02-20 00:18:23 -0300

    for loop and css created

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const app = express()
 const port = 8080
 
 app.set('view engine','ejs')
+app.use(express.static('public'))
 
 app.get("/",(req,res) => {
     let name = "Guilherme Koller"
-
+    let produto = [
+        {nome:"coca", preco: 2},
+        {nome:"pepsi", preco: 3},
+        {nome:"dolly", preco: 1},
+        {nome:"cini", preco: 28},
+        {nome:"itubaina", preco: 2},
+        {nome:"fanta", preco: 1.3}
+    ]
     res.render("index", {
-        name
+        name,
+        produto
     })
 })
 
