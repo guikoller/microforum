5b82f2a9bc33b0b280dc32bf58b4aa4a3e60f0bc
guikoller <g_koller@hotmail.com>
2022-02-24 12:01:58 -0300

    ask form created

--- index.js
+++ index.js
@@ -6,21 +6,12 @@ app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.get("/",(req,res) => {
-    let name = "Guilherme Koller"
-    let produto = [
-        {nome:"coca", preco: 2},
-        {nome:"pepsi", preco: 3},
-        {nome:"dolly", preco: 1},
-        {nome:"cini", preco: 28},
-        {nome:"itubaina", preco: 2},
-        {nome:"fanta", preco: 1.3}
-    ]
-    res.render("index", {
-        name,
-        produto
-    })
+    res.render("index")
 })
 
-app.listen(port, () => {
-    console.log(`Application running in the port ${port}`)
+
+app.get("/ask",(req,res) => {
+    res.render("ask")
 })
+
+app.listen(port, () => console.log(`Application running in the port ${port}`))
