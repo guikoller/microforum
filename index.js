6f494156782047b475b85db62ebee02b108385ba
guikoller <g_koller@hotmail.com>
2022-02-27 15:52:51 -0300

    ask post route

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const port = 8080
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 app.get("/",(req,res) => {
     res.render("index")
 })
 
-
 app.get("/ask",(req,res) => {
     res.render("ask")
+})
+
+app.post("/ask", (req, res) => {
+    let data = req.body;
+    console.log(data)
+    res.send('form received')
 })
 
 app.listen(port, () => console.log(`Application running at the port ${port}`))
